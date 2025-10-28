749e437063e63eebbd809397dadc011e4000d7b9
Vin <vlou1@nait.ca>
2025-10-28 17:37:49 -0600

    Test hasCity, should fail

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,15 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-public class CustomListTest {
+public class CustomListTest
+{
+	@Test
+	public void testHasCity()
+	{
+		CustomList list = new CustomList();
+		City c = new City("Calgary", "ab");
+		list.addCity(c);
 
+		assertTrue(list.hasCity(c));
+	}
 }
